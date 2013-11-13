02c7245b949624b2151a6ffc814d4b81e5cbc7d1
Mr. Lawrence <lawrence@server.fake>
2013-11-13 18:40:19 +0100

    not working yet

---
+++ src/Philosoph.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Philosoph implements Runnable {
+	private ReentrantLock linkeHand;
+	private ReentrantLock rechteHand;
+	
+	private boolean istHungrig = false;
+
+	static AtomicInteger nextId = new AtomicInteger();
+	private Integer id;
+
+	public Philosoph() {
+		this.id = nextId.incrementAndGet();
+	}
+
+	@Override
+	public void run() {
+
+	}
+}
